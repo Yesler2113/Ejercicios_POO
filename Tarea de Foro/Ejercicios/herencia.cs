c5e31ea78d8637a4a52a3e5ef06fa32eb49d8d2a
Yesler2113 <diazmunozdm12@gmail.com>
2023-06-20 11:46:39 -0600

    Agregar archivos de proyecto.

---
+++ Tarea de Foro/Ejercicios/herencia.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_de_Foro.Ejercicios
+{
+    public class herencia
+    {
+      
+    }
+    class Transporte
+    {
+        public string Nombre { get; set;}
+        public String Marca {get; set;}
+
+        public void Arrancar()
+        {
+            Console.WriteLine("EL medio de transporte esta arrancando.");
+
+        }
+    }
+
+    class Avion : Transporte
+    {
+        public void Despegando()
+        {
+            Console.WriteLine("El avion ya despego.");
+        }
+    }
+
+    class Carro : Transporte
+    {
+        public void Conducir()
+        {
+            Console.WriteLine("El carro ya va  corriendo. ");
+        }
+    }
+}
---
+++ Tarea de Foro/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tarea_de_Foro.Ejercicios;
+
+Avion avion = new Avion();
+avion.Nombre = "Avion";
+avion.Marca = "Spirit";
+Console.WriteLine("Nombre del transporte: " + avion.Nombre);
+Console.WriteLine("Marca: "+ avion.Marca);
+avion.Arrancar();
+avion.Despegando();
+
+
+
+Console.WriteLine();
+
+Carro micarro = new Carro();
+micarro.Nombre = "Carro";
+micarro.Marca = "Toyota.";
+Console.WriteLine("Nombre del Transporte: " + micarro.Nombre);
+Console.WriteLine("Marca: " + micarro.Marca);
+micarro.Arrancar();
+micarro.Conducir();
